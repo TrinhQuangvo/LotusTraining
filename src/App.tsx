cff569638295ad34a3a3e2f75324353c1ed54647
Quang.Fe.Dev <voquang1406@gmail.com>
2022-03-15 00:04:38 +0700

    set up completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import Checkout from './Components/Checkout/'
 import './App.scss'
 
 const App = () => {
   return (
     <div className='app'>
-      <p>React is Running</p>
+      <p>React is Running sad sad
+      </p>
+      <Checkout />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/Checkout/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './style.scss'
+
+const Checkout: React.FC = () => {
+  return (
+    <div>Check Out Components  </div>
+  )
+}
+export default Checkout
