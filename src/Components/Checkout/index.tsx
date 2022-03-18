c41883519ac42334f9119e25397ca6484a882c94
MinhChau2910 <79617408+MinhChau2910@users.noreply.github.com>
2022-03-18 09:33:12 +0700

    no message

--- src/Components/Checkout/index.tsx
+++ src/Components/Checkout/index.tsx
@@ -10,22 +10,27 @@ interface CheckoutProps {
 const Checkout: FC<CheckoutProps> = ({ data }) => {
   return (
     <FullWidthContainer>
-      <>
-        <h1 className="checkout-index-title">Checkout</h1>
-        <div className="checkout">
-          <img src={placeholderImage} />
-          {!!data
-            ? data.map((item: any) => {
-              return (
-                <div className="checkout-container">
+      <h1 className="checkout-index-title">Checkout</h1>
+      {/* {!!data
+          ? data.map((item: any, index: number) => { */}
+      <div className="checkout">
+        {/* <img src={placeholderImage} /> */}
+        {!!data ? (
+          data.map((item: any, index: number) => {
+            return (
+              <div key={index} className="checkout-container">
+                <img src={placeholderImage} />
+                <div>
                   <form className="checkout-container-order">
-                    <h3>Đơn hàng</h3>
+                    <h3>My Order</h3>
                     <p className="checkout-index-name">Name Produce: {item.name}</p>
                     <p className="checkout-index-price">
                       Price Produce: {item.price}
                       {item.currency}
                     </p>
                   </form>
+                </div>
+                <div>
                   <form className="checkout-container-details">
                     <h3>Shipment Details</h3>
                     <label htmlFor="">Name</label> <br />
@@ -35,33 +40,39 @@ const Checkout: FC<CheckoutProps> = ({ data }) => {
                     <input type="text" placeholder="Phone number" />
                   </form>
                 </div>
-              );
-            })
-            : ''}
-          <form className="checkout-container-details">
-            <label htmlFor="">Address: </label> <br />
-            <input type="text" placeholder="Address" />
-            <br />
-            <label htmlFor="">Payment methods: </label> <br />
-            <select className="form-select">
-              <option selected>Cash</option>
-              <option value="1">Transfer</option>
-              <option value="2">Credit</option>
-            </select>
-            <div className="details_btn">
-              <Button isFullWidth={false} status="danger">
-                Office
-              </Button>
-              <Button isFullWidth={false} status="danger">
-                Home
-              </Button>
-            </div>
-            <Button status="danger" isFullWidth={true}>
-              Save
-            </Button>
-          </form>
-        </div>
-      </>
+                <div>
+                  <form className="checkout-container-details">
+                    <label htmlFor="">Address: </label> <br />
+                    <input type="text" placeholder="Address" />
+                    <br />
+                    <label htmlFor="">Payment methods: </label> <br />
+                    <select className="form-select">
+                      <option selected>Cash</option>
+                      <option value="1">Transfer</option>
+                      <option value="2">Credit</option>
+                    </select>
+                    <div className="details_btn">
+                      <Button isFullWidth={false} status="danger">
+                        Office
+                      </Button>
+                      <Button isFullWidth={false} status="danger">
+                        Home
+                      </Button>
+                    </div>
+                    <Button status="danger" isFullWidth={true}>
+                      Save
+                    </Button>
+                  </form>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <Button isFullWidth={false} status="success">
+            Trở Về Trang Mua Hàng
+          </Button>
+        )}
+      </div>
     </FullWidthContainer>
   );
 };
